c82146abdb916e9dd3032582c012a4fe1daca82b
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2018-03-24 20:16:46 +0900

    add post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -3,11 +3,28 @@ class Admin::PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      flash[:success] = 'asdsdf'
+      redirect_to admin_posts_path
+    else
+      flash[:danger]  = 'dafsdf'
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  protected
+  def post_params
+    params.require(:post).permit(:title, :content, :category, :cover)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :lockable, :recoverable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :lockable, :recoverable
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
