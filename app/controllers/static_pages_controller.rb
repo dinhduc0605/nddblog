3aac1b7f0c5a57c9c5f239c6f92f1578048eb2b0
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2018-04-07 22:56:08 +0900

    recreate homepage with clean-blog template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
-  layout false, only: :home
   def home
     @new_posts = Post.limit(5)
     @hot_posts = Post.limit(5)
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   resources :posts
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
