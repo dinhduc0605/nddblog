e8f683cbe69f31920b4998333d04b11906a1c11e
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2018-12-10 22:21:02 +0900

    Fix bug not show all tag of post in tag page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,9 +3,9 @@ class TagsController < ApplicationController
 
   def show
     if params[:post_id]
-      @posts = Post.includes(:tags).where("posts.id < ?", params[:post_id]).where(:tags => { id: @tag.id }).order(created_at: :desc).limit(5)
+      @posts = Post.joins(:tags).where("posts.id < ?", params[:post_id]).where(:tags => { id: @tag.id }).order(created_at: :desc).limit(5)
     else
-      @posts = Post.includes(:tags).where(:tags => { id: @tag.id }).order(created_at: :desc).limit(5)
+      @posts = Post.joins(:tags).where(:tags => { id: @tag.id }).order(created_at: :desc).limit(5)
     end
     respond_to do |format|
       format.html
