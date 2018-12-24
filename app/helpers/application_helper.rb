ffd6bd58ac0ba8b7dbb1895d7152332565dc623f
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2018-12-24 13:52:44 +0900

    Use github-mardown css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,14 +29,15 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    rndr = MarkdownRenderer.new(:hard_wrap => true)
+    rndr = MarkdownRenderer.new(hard_wrap: true)
     options = {
-        :fenced_code_blocks => true,
-        :no_intra_emphasis => true,
-        :autolink => true,
-        :strikethrough => true,
-        :lax_html_blocks => true,
-        :superscript => true
+        fenced_code_blocks: true,
+        no_intra_emphasis: true,
+        autolink: true,
+        strikethrough: true,
+        lax_html_blocks: true,
+        superscript: true,
+        tables: true
     }
     markdown_to_html = Redcarpet::Markdown.new(rndr, options)
     markdown_to_html.render(text).html_safe
