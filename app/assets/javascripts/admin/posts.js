77a147b2d9f379df9703ca342cbe2d24aa12ca8b
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2019-02-11 01:58:54 +0900

    Add markdown editor

--- app/assets/javascripts/admin/posts.js
+++ app/assets/javascripts/admin/posts.js
@@ -1,38 +1,24 @@
 $(document).on('turbolinks:load', function () {
-    $.uploadPreview({
-        input_field: "#post_cover",   // Default: .image-upload
-        preview_box: "#background-preview",  // Default: .image-preview
-        no_label: true                 // Default: false
-    });
+  $.uploadPreview({
+    input_field: "#post_cover",   // Default: .image-upload
+    preview_box: "#background-preview",  // Default: .image-preview
+    no_label: true                 // Default: false
+  });
 
-    $("#form-tags").tagit({
-        singleField: true,
-        autocomplete: {
-            source: function(search, showChoices) {
-                $.ajax({
-                    url: "/admin/tags.json",
-                    data: search,
-                    success: function(choices) {
-                        showChoices(choices.map(function (choice) {
-                            return choice.name;
-                        }));
-                    }
-                });
-            }
-        }
-    });
-});
-
-$(document).on('click', '#edit-with-stackedit', function () {
-    const textareaEl = document.querySelector('textarea');
-    const stackedit = new Stackedit();
-    stackedit.on('fileChange', function onFileChange(file) {
-        textareaEl.value = file.content.text;
-    });
-    stackedit.openFile({
-        name: 'Markdown with StackEdit',
-        content: {
-            text: textareaEl.value
-        }
-    });
+  $("#form-tags").tagit({
+    singleField: true,
+    autocomplete: {
+      source: function (search, showChoices) {
+        $.ajax({
+          url: "/admin/tags.json",
+          data: search,
+          success: function (choices) {
+            showChoices(choices.map(function (choice) {
+              return choice.name;
+            }));
+          }
+        });
+      }
+    }
+  });
 });
