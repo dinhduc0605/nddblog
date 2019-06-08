82b62de45feac0dfb770a02a01ae08d05bcd8e7c
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2019-06-09 01:58:51 +0900

    Notify error to slack

---
+++ lib/slack_notifier.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module SlackNotifier
+  def self.ping_errors(e, location = nil)
+    client = Slack::Web::Client.new
+    attachments = [
+      {
+        "title": "Error message",
+        "text": (location.present? ? "URL: #{location}\n" : '') + e.message,
+        "color": "#FF0000",
+        "footer": "Bellow is full traces",
+        "ts": Time.current.to_i
+      }
+    ]
+    client.chat_postMessage(channel: ENV['ERROR_CHANNEL_SLACK'], as_user: true, attachments: attachments.to_json)
+    ping_fulltrace(e)
+  end
+
+  def self.ping_fulltrace(e)
+    client = Slack::Web::Client.new
+    error_trace = Rails.backtrace_cleaner.clean(e.backtrace)
+    error_trace = error_trace.join("\n")
+    client.chat_postMessage(channel: ENV['ERROR_CHANNEL_SLACK'], as_user: true, text: "```\n" + error_trace + "\n```")
+  end
+end
