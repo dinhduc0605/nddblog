63cdcaf3a964a23ec4cf704cca366752463df1d9
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2020-05-06 20:10:45 +0900

    remove unused files

--- app/assets/javascripts/sw_reg.js
+++ app/assets/javascripts/sw_reg.js
@@ -1,8 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js', { scope: './' })
-        .then((reg) => {
-          console.log('Service worker registered.', reg);
-        });
+        .then((reg) => {});
   });
 }
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.precompile += %w[*.js *.css **/*.js **/*.css jquery3.js skill_progress.js turbolinks.js]
+Rails.application.config.assets.precompile += %w[*.js *.css **/*.js **/*.css jquery3.js turbolinks.js]
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
