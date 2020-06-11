18b91ad7c6647fca7f635d3520afc35ece6e8edc
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2020-06-12 00:56:39 +0900

    fix rubocop script

--- sh/rubocop.sh
+++ sh/rubocop.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-if [ "${CIRCLE_BRANCH}" != "master" ]; then
-    git diff -z --name-only origin/master \
-     | xargs -0 bundle exec rubocop-select \
-     | xargs bundle exec rubocop \
-         --require rubocop/formatter/checkstyle_formatter \
-         --format RuboCop::Formatter::CheckstyleFormatter \
-     | bundle exec checkstyle_filter-git diff origin/master \
-     | bundle exec saddler report \
-        --require saddler/reporter/github \
-        --reporter Saddler::Reporter::Github::PullRequestReviewComment
+if [ "${CIRCLE_BRANCH}" != "develop" ] && [ "${CIRCLE_BRANCH}" != "master" ]; then
+    warn=$(git diff -z --name-only origin/master \
+           | xargs -0 bundle exec rubocop-select \
+           | xargs bundle exec rubocop \
+              --require rubocop/formatter/checkstyle_formatter \
+              --format RuboCop::Formatter::CheckstyleFormatter \
+           | bundle exec checkstyle_filter-git diff origin/master)
+    detected=$(echo "$warn" | grep "<error")
+    if [ -n "$detected" ]; then
+        echo "$warn" \
+        | bundle exec saddler report \
+            --reporter Saddler::Reporter::Text
+        exit 1
+    fi
 fi
-
 exit 0
