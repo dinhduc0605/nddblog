5632d53492c7c31205424cc3395edecf7f24a64e
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2023-02-28 15:46:35 +0900

    make image url not expire

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -21,6 +21,4 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region:                ENV['AWS_REGION']
   }
-  config.fog_public     = false 
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
